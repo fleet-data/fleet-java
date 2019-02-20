ce33f91032b71a2831bbc5a552607c44219bfeb7
Brett Griffin <brett@deepchannel.com>
2019-02-20 00:47:18 -0500

    Implment ValidatableConnection

--- src/main/java/com/deepchannel/fleet/source/salesforce/Connection.java
+++ src/main/java/com/deepchannel/fleet/source/salesforce/Connection.java
@@ -1,10 +1,26 @@
 package com.deepchannel.fleet.source.salesforce;
 
-public class Connection {
+import com.deepchannel.fleet.common.logging.TaskLog;
+import com.deepchannel.fleet.common.task.ConnectionConnectResult;
+import com.deepchannel.fleet.common.task.ValidatableConnection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Connection implements ValidatableConnection {
 
     public String clientId;
 
     public String clientSecret;
 
     public String refreshToken;
+
+    @Override
+    public ConnectionConnectResult attempConnect() {
+        List<TaskLog> logs = new ArrayList<>();
+        logs.add(new TaskLog("INFO", "Connceting to establish connection..."));
+        logs.add(new TaskLog("INFO", "Connection etablished..."));
+
+        return new ConnectionConnectResult(true, logs);
+    }
 }
