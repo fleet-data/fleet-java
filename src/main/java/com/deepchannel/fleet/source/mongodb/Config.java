140d5f95c5523b256478dade8e7a90961560b659
Brett Griffin <brett@deepchannel.com>
2019-02-21 00:50:13 -0500

    Add MongoDb

---
+++ src/main/java/com/deepchannel/fleet/source/mongodb/Config.java
@@ -0,0 +1,11 @@
+package com.deepchannel.fleet.source.mongodb;
+
+public class Config {
+
+    public String collection;
+
+    public String identityField;
+
+    public String updateField;
+
+}
---
+++ src/main/java/com/deepchannel/fleet/source/mongodb/Connection.java
@@ -0,0 +1,39 @@
+package com.deepchannel.fleet.source.mongodb;
+
+import com.deepchannel.fleet.common.logging.TaskLog;
+import com.deepchannel.fleet.common.task.ConnectionConnectResult;
+import com.deepchannel.fleet.common.task.ValidatableConnection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Connection implements ValidatableConnection {
+
+    public String host;
+
+    public int port;
+
+    public String database;
+
+    public String username;
+
+    public String password;
+
+    public boolean useSsh;
+
+    public String sshHost;
+
+    public int sshPort;
+
+    public String sshUser;
+
+    @Override
+    public ConnectionConnectResult attemptConnect() {
+        List<TaskLog> logs = new ArrayList<>();
+        logs.add(new TaskLog("INFO", "Connceting to establish connection..."));
+        logs.add(new TaskLog("INFO", "Connection etablished..."));
+
+        return new ConnectionConnectResult(true, logs);
+    }
+
+}
