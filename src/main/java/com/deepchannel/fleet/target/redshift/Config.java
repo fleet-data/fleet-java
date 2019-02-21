79e763b8dad4617bc95f561b3fcb0a5a2ec486d8
Brett Griffin <brett@deepchannel.com>
2019-02-20 23:41:20 -0500

    Add Config

---
+++ src/main/java/com/deepchannel/fleet/target/redshift/Config.java
@@ -0,0 +1,15 @@
+package com.deepchannel.fleet.target.redshift;
+
+public class Config {
+
+    public String schema;
+
+    public String table;
+
+    public String mode;
+
+    public String mergeKeys;
+
+    public String updateColumn;
+
+}
