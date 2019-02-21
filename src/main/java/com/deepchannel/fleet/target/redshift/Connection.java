90b8fa99848c6bc50258aa892d39f9fda841d347
Brett Griffin <brett@deepchannel.com>
2019-02-21 00:57:59 -0500

    Fix logs

--- src/main/java/com/deepchannel/fleet/target/redshift/Connection.java
+++ src/main/java/com/deepchannel/fleet/target/redshift/Connection.java
@@ -30,8 +30,8 @@ public class Connection implements ValidatableConnection {
     @Override
     public ConnectionConnectResult attemptConnect() {
         List<TaskLog> logs = new ArrayList<>();
-        logs.add(new TaskLog("INFO", "Connceting to establish connection..."));
-        logs.add(new TaskLog("INFO", "Connection etablished..."));
+        logs.add(new TaskLog("INFO", "Attempting to establish connection to Redshift..."));
+        logs.add(new TaskLog("INFO", "Connection established"));
 
         return new ConnectionConnectResult(true, logs);
     }
