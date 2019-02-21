5dfc38391522814611e7b9d5c769019405f79ac5
Brett Griffin <brett@deepchannel.com>
2019-02-21 01:29:11 -0500

    Add ReCharge

---
+++ src/main/java/com/deepchannel/fleet/source/recharge/Config.java
@@ -0,0 +1,6 @@
+package com.deepchannel.fleet.source.recharge;
+
+public class Config {
+
+    public String entity;
+}
---
+++ src/main/java/com/deepchannel/fleet/source/recharge/Connection.java
@@ -0,0 +1,22 @@
+package com.deepchannel.fleet.source.recharge;
+
+import com.deepchannel.fleet.common.logging.TaskLog;
+import com.deepchannel.fleet.common.task.ConnectionConnectResult;
+import com.deepchannel.fleet.common.task.ValidatableConnection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Connection implements ValidatableConnection {
+
+    public String accessToken;
+
+    @Override
+    public ConnectionConnectResult attemptConnect() {
+        List<TaskLog> logs = new ArrayList<>();
+        logs.add(new TaskLog("INFO", "Attempting to establish connection to ReCharge..."));
+        logs.add(new TaskLog("INFO", "Connection established"));
+
+        return new ConnectionConnectResult(true, logs);
+    }
+}
