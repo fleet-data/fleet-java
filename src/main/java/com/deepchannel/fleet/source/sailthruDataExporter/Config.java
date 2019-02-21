fe47f11a57753b8f89ef7c5008512ca110475205
Brett Griffin <brett@deepchannel.com>
2019-02-21 01:32:25 -0500

    Add Sailthru

---
+++ src/main/java/com/deepchannel/fleet/source/sailthruDataExporter/Config.java
@@ -0,0 +1,7 @@
+package com.deepchannel.fleet.source.sailthruDataExporter;
+
+public class Config {
+
+    public String entity;
+
+}
---
+++ src/main/java/com/deepchannel/fleet/source/sailthruDataExporter/Connection.java
@@ -0,0 +1,30 @@
+package com.deepchannel.fleet.source.sailthruDataExporter;
+
+import com.deepchannel.fleet.common.logging.TaskLog;
+import com.deepchannel.fleet.common.task.ConnectionConnectResult;
+import com.deepchannel.fleet.common.task.ValidatableConnection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Connection  implements ValidatableConnection {
+
+    public String accountId;
+
+    public String bucketName;
+
+    public String awsRegionName;
+
+    public String awsAccessKeyId;
+
+    public String awsSecretAccessKey;
+
+    @Override
+    public ConnectionConnectResult attemptConnect() {
+        List<TaskLog> logs = new ArrayList<>();
+        logs.add(new TaskLog("INFO", "Attempting to establish connection to Sailthru..."));
+        logs.add(new TaskLog("INFO", "Connection established"));
+
+        return new ConnectionConnectResult(true, logs);
+    }
+}
