26a422fd5ba51a14ec969423004c6f32e4c0314a
Brett Griffin <brett@deepchannel.com>
2019-02-21 01:52:15 -0500

    Example code to test connection

--- src/main/java/com/deepchannel/fleet/FleetRunner.java
+++ src/main/java/com/deepchannel/fleet/FleetRunner.java
@@ -1,8 +1,17 @@
 package com.deepchannel.fleet;
 
+import com.deepchannel.fleet.common.logging.TaskLog;
+import com.deepchannel.fleet.common.task.ConnectionConnectResult;
+import com.deepchannel.fleet.common.task.ValidatableConnection;
+
 public class FleetRunner {
 
     public static void main(String[] args) {
+        ValidatableConnection con = new com.deepchannel.fleet.target.snowflake.Connection();
+        ConnectionConnectResult connectionConnectResult = con.attemptConnect();
 
+        for (TaskLog tl : connectionConnectResult.logs) {
+            System.out.println(tl.message);
+        }
     }
 }
