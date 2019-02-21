4019997612b92b28038650847ae2492a3df1d66d
Brett Griffin <brett@deepchannel.com>
2019-02-21 01:26:19 -0500

    Add iTunes Connect Config

--- src/main/java/com/deepchannel/fleet/source/itunesConnect/Config.java
+++ src/main/java/com/deepchannel/fleet/source/itunesConnect/Config.java
@@ -1,4 +1,15 @@
 package com.deepchannel.fleet.source.itunesConnect;
 
 public class Config {
+
+    public String appId;
+
+    public String interval;
+
+    public String measures;
+
+    public String dimension;
+
+    public String filters;
+    
 }
