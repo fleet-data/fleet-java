cb8dbd9b57c88432af179e9bdae20372c4525d85
Brett Griffin <brett@deepchannel.com>
2019-04-16 11:45:13 -0400

    Add properties to config

--- src/main/java/com/deepchannel/fleet/source/sftp/Config.java
+++ src/main/java/com/deepchannel/fleet/source/sftp/Config.java
@@ -1,4 +1,23 @@
 package com.deepchannel.fleet.source.sftp;
 
 public class Config {
+
+    public String folderPath;
+
+    public String fileNamePattern;
+
+    public String fileType;
+
+    public String delimter;
+
+    public String compressionType;
+
+    public String errorHandlingType;
+
+    public boolean useLatestFile;
+
+    public int skipHeaderLines;
+
+    public int skipFooterLines;
+
 }
