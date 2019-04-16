e53ddb0b864701531e4fbf68981c1956e88a5004
Brett Griffin <brett@deepchannel.com>
2019-04-16 11:48:54 -0400

    Add properties to File Upload Config

--- src/main/java/com/deepchannel/fleet/source/fileUpload/Config.java
+++ src/main/java/com/deepchannel/fleet/source/fileUpload/Config.java
@@ -1,4 +1,18 @@
 package com.deepchannel.fleet.source.fileUpload;
 
 public class Config {
+
+    public String fileLocation;
+
+    public String fileType;
+
+    public String delimter;
+
+    public String errorHandlingType;
+
+    public int skipHeaderLines;
+
+    public int skipFooterLines;
+
+    public Object schema;
 }
