439821ef72815c0269c124602ab219b24befcfb3
Brett Griffin <brett@deepchannel.com>
2019-04-16 11:07:49 -0400

    Add DataStoreColumn

---
+++ src/main/java/com/deepchannel/fleet/common/DataStoreColumn.java
@@ -0,0 +1,23 @@
+package com.deepchannel.fleet.common;
+
+public class DataStoreColumn {
+
+    public String fullName;
+
+    public String databaseName;
+
+    public String type;
+
+    public int maxLength;
+
+    public int precision;
+
+    public int scale;
+
+    public boolean nullable;
+
+    public int index;
+
+    public Object defaultValue;
+
+}
