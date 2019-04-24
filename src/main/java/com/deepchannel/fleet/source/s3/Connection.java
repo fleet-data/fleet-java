3caf64677990714187b04f5776636112dc3cc45f
Brett Griffin <brett@deepchannel.com>
2019-04-24 00:24:56 -0400

    Change bucketName to awsBucketName

--- src/main/java/com/deepchannel/fleet/source/s3/Connection.java
+++ src/main/java/com/deepchannel/fleet/source/s3/Connection.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public class Connection implements ValidatableConnection {
 
-    public String bucketName;
+    public String awsBucketName;
 
     public String awsRegionName;
 
